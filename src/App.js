685e2a95e28d3d71fa6140de80ed34609413dd4f
Malikil <eboy.coltt5@gmail.com>
2019-04-02 18:19:04 -0700

    Add fetch to authentication api endpoint

--- src/App.js
+++ src/App.js
@@ -9,7 +9,34 @@ function Home() {
   return <h2>Movie Theatre</h2>;
 }
 
-class App extends React.Component {
+class App extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+
+    this.state = {
+      loggedInAs: null
+    };
+  }
+
+  login(user, pass)
+  {
+    fetch('http://localhost:1337/auth',
+    {
+      method: "GET",
+      headers: {
+        "x-username": user,
+        "x-password": pass
+      }
+    })
+    .then(response => response.json())
+    .then(data => 
+      this.setState({
+        loggedInAs: data.userid
+      }));
+  }
+
   render() {
     return <div>
       <Navbar />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Cookies from 'js-cookie';
 
 
 // https://stackoverflow.com/questions/49819183/react-what-is-the-best-way-to-handle-authenticated-logged-in-state
@@ -17,46 +16,6 @@ class Navbar extends React.Component
             isAuthenticated: false,
             login: this.login
         };
-    }
-
-    isAuthenticated()
-    {
-        export const getAccessToken = () => Cookies.get('access_token');
-        export const getRefreshToken = () => Cookies.get('refresh_token');
-        export const isAuthenticated = () => !!getAccessToken();
-    }
-
-    authenticate()
-    {
-        export const authenticate = async () => {
-            if (getRefreshToken()) {
-              try {
-                const tokens = await refreshTokens() // call an API, returns tokens
-          
-                const expires = (tokens.expires_in || 60 * 60) * 1000
-                const inOneHour = new Date(new Date().getTime() + expires)
-          
-                // you will have the exact same setters in your Login page/app too
-                Cookies.set('access_token', tokens.access_token, { expires: inOneHour })
-                Cookies.set('refresh_token', tokens.refresh_token)
-          
-                return true
-              } catch (error) {
-                redirectToLogin()
-                return false
-              }
-            }
-          
-            redirectToLogin()
-            return false
-          }
-    }
-
-    redirectToLogin()
-    {
-        const redirectToLogin = () => {
-            history.push('/login')
-          }
     }
 
     componentDidMount() {
